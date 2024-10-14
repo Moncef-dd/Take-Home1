221a9d00801b60da26ae37e5d1ab49b8b0fe9119
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2024-10-14 01:40:13 -0500

    problem description + Book constructor && 2 book methods

---
+++ main.js
@@ -0,0 +1,32 @@
+// interveiw question
+/*
+Implement the system of an online cloud reading platform. 
+ - the users have a library they can add books to or emove from. 
+ - Users can set a book from their library as active
+ - the application remember where a user left off in a given book. 
+ 
+ - it displays only one page at a time. 
+
+*/
+
+class Book {
+    constructor(bookID, title, data) {
+        this.bookID = bookID; 
+        this.title = title; 
+        this.data = data; // a list of strings
+
+        this.seen = false; 
+        this.currPage = 0;
+    }
+
+    pageDisplay() {
+        if(!this.seen && this.data){
+            this.currPage = 1; 
+            return this.data[this.currPage]; 
+        } else {
+
+        }
+
+    }
+
+}
